1d281d5ad40294e59778f990ce3a969a74826ba2
Ricardo Castanho <48383095+ricardocastanho@users.noreply.github.com>
2023-06-23 18:50:21 -0300

    feat: add graphql subscriptions

--- index.js
+++ index.js
@@ -4,17 +4,15 @@ import cors from "cors";
 import neo4j from "neo4j-driver";
 import { WebSocketServer } from "ws";
 import { createServer } from "http";
-import { createClient } from "redis";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
-// import { ApolloServerPluginLandingPageGraphQLPlayground } from "@apollo/server-plugin-landing-page-graphql-playground";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import { useServer } from "graphql-ws/lib/use/ws";
+import { RedisPubSub } from "graphql-redis-subscriptions";
 import {
   Neo4jGraphQL,
   Neo4jGraphQLSubscriptionsSingleInstancePlugin,
 } from "@neo4j/graphql";
-import { EventEmitter } from "events";
 
 const HOST = "localhost";
 const PATH = "/graphql";
@@ -22,7 +20,17 @@ const PORT = 4001;
 const NEO4J_URL = "bolt://34.232.46.151:7687";
 const NEO4J_USER = "neo4j";
 const NEO4J_PASSWORD = "itinerary-gallon-holddown";
-const REDIS_URL = "redis://root:root@localhost:6357";
+const REDIS_URL = "redis://default:default@localhost:6379";
+const EVENT_KEY = "NOTIFICATION";
+
+const driver = neo4j.driver(
+  NEO4J_URL,
+  neo4j.auth.basic(NEO4J_USER, NEO4J_PASSWORD)
+);
+
+const pubsub = new RedisPubSub({
+  connection: REDIS_URL,
+});
 
 const typeDefs = `
   type Movie {
@@ -37,53 +45,39 @@ const typeDefs = `
     name: String!
     movies: [Movie!]! @relationship(type: "ACTED_IN", direction: IN)
   }
+
+  type Mutation {
+    notify: String!
+  }
+
+  type Subscription {
+    notification: String!
+  }
 `;
 
-const driver = neo4j.driver(
-  NEO4J_URL,
-  neo4j.auth.basic(NEO4J_USER, NEO4J_PASSWORD)
-);
-
-class CustomRedisSubscriptionPlugin {
-  constructor(redisClient) {
-    this.client = redisClient;
-    this.events = new EventEmitter();
-  }
-
-  async init() {
-    this.subscriber = this.client.duplicate();
-    this.publisher = this.client.duplicate();
-    await this.subscriber.connect();
-    await this.publisher.connect();
-    await this.subscriber.subscribe("graphql-api-subscriptions", (message) => {
-      const eventMeta = JSON.parse(message);
-      this.events.emit(eventMeta.event, eventMeta);
-    });
-  }
-
-  async publish(eventMeta) {
-    await this.publisher.publish(
-      "graphql-api-subscriptions",
-      JSON.stringify(eventMeta)
-    );
-  }
-}
-
-const neo4jGraphQL = new Neo4jGraphQL({
-  typeDefs,
-  driver,
-  plugins: {
-    subscriptions: new Neo4jGraphQLSubscriptionsSingleInstancePlugin(),
+const resolvers = {
+  Mutation: {
+    notify: async () => {
+      await pubsub.publish(EVENT_KEY, { notification: "New movie launched!" });
+      return "Notification sent!";
+    },
   },
-});
+  Subscription: {
+    notification: {
+      subscribe: () => pubsub.asyncIterator(EVENT_KEY),
+    },
+  },
+};
 
 async function main() {
-  // const client = createClient(REDIS_URL);
-  // client.on("error", (err) => console.log("Redis Client Error", err));
-  // await client.connect();
-
-  // const redisSubscriptions = new CustomRedisSubscriptionPlugin(client);
-  // await redisSubscriptions.init();
+  const neo4jGraphQL = new Neo4jGraphQL({
+    typeDefs,
+    resolvers,
+    driver,
+    plugins: {
+      subscriptions: new Neo4jGraphQLSubscriptionsSingleInstancePlugin(),
+    },
+  });
 
   const app = express();
   const httpServer = createServer(app);
@@ -108,7 +102,6 @@ async function main() {
     schema: schema,
     playground: true,
     plugins: [
-      // ApolloServerPluginLandingPageGraphQLPlayground(),
       ApolloServerPluginDrainHttpServer({
         httpServer,
       }),
